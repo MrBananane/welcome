77baca1538dc81985bf4a5013205a466623533e4
Noam Silvy <noam.silvy@epitech.eu>
2017-05-31 13:07:30 +0200

    Add swag

--- app/controllers/hub_booking_controller.rb
+++ app/controllers/hub_booking_controller.rb
@@ -1,4 +1,6 @@
 class HubBookingController < ApplicationController
+  before_action :create_google_client, only: [:calendars, :events]
+
   def redirect
     client = Signet::OAuth2::Client.new(
       {
@@ -28,16 +30,23 @@ class HubBookingController < ApplicationController
   end
 
   def calendars
+    @calendar_list = @service.list_calendar_lists
+  end
+
+  def events
+    @event_list = @service.list_events(params[:calendar_id])
+  end
+
+  private
+  def create_google_client
     client = Signet::OAuth2::Client.new(
       {
         client_id: Rails.application.secrets.google_client_id,
         client_secret: Rails.application.secrets.google_client_secret,
-        authorization_uri: 'https://accounts.google.com/o/oauth2/token'
+        token_credential_uri: 'https://accounts.google.com/o/oauth2/token'
       })
-
     client.update!(session[:authorization])
-    service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = client
-    @calendar_list = service.list_calendar_lists
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    @service.authorization = client
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/redirect', to: 'hub_booking#redirect', as: 'redirect'
   get '/callback', to: 'hub_booking#callback', as: 'callback'
   get '/calendars', to: 'hub_booking#calendars', as: 'calendars'
+  get 'events/:calendar_id', to: 'hub_booking#events', as: 'events', calendar_id: /[^\/]+/
 end
